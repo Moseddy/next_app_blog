23316523d80ca8062bd3a898ab70e3256446c5ab
Moseddy <moseddy1@gmail.com>
2023-11-15 14:12:33 +0100

    completed the home page, about page, contact and the porfolio page using the nextjs page.module.style, layout, Image and Link

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import styles from './page.module.css'
+import Image from 'next/image';
+import Button from '@/components/Button/Button';
 
 const About = () => {
   return (
-    <div className={styles.container}> About</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="https://images.pexels.com/photos/3314328/pexels-photo-3314328.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+        fill={true}
+        className={styles.img}  />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storyteller</h1>
+          <h2 className={styles.imgDesc}>Handcrafting award-winning digital experiences</h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+          <div className={styles.item}>
+            <h1 className={styles.title}> Who we are</h1>
+            <p className={styles.description}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias delectus maxime illo consequatur, quidem voluptatibus eius odit nobis, dicta deserunt veniam esse ipsam? <br /> <br /> 
+              In numquam molestiae aliquid autem placeat dignissimos tempore minima, ex laborum mollitia, doloremque harum? Animi, consectetur aspernatur.
+
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias delectus maxime illo consequatur, quidem voluptatibus eius odit nobis, dicta deserunt veniam esse ipsam? <br /> <br /> 
+              In numquam molestiae aliquid autem placeat dignissimos tempore minima, ex laborum mollitia, doloremque harum? Animi, consectetur aspernatur.
+            </p>
+          </div>
+
+          <div className={styles.item}>
+            <h1 className={styles.title}> What we do</h1>
+            <p className={styles.description}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias delectus maxime illo consequatur, quidem voluptatibus eius odit nobis, dicta deserunt veniam esse ipsam?
+              quidem voluptatibus eius odit nobis, dicta deserunt veniam esse ipsam? <br /> <br /> 
+              Animi, consectetur aspernatur.
+            </p>
+            <Button url={'/contact'} text={'Contact Us'} />
+          </div>
+        </div>
+    </div>
   )
 }
 
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import styles from './page.module.css';
+import Image from 'next/image';
+import Button from '@/components/Button/Button';
+import contactImg from 'public/contact.png'
 
 const Contact = () => {
   return (
-    <div className={styles.contact}>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        Let's Keep in Touch
+      </h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src={contactImg} fill={true} className={styles.img} />
+        </div>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder='Enter your name' className={styles.input} />
+          <input type="email" placeholder='Enter your email' className={styles.input} />
+          <textarea 
+            name="" 
+            id="" 
+            cols="30" 
+            rows="10"
+            placeholder='Enter your message'
+            className={styles.textarea}>
+
+            </textarea>
+
+            <Button text={"Submit"} url={'#'} />
+        </form>
+      </div>
+    </div>
   )
 }
 
